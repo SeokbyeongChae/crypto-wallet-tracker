50cd60ab2897827fe89bac1cccfe4e75c6fd6b76
seokbyeongchae <wkrlzzz@naver.com>
2024-04-09 01:57:34 +0900

    project setup

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/seokbyeongchae/crypto-wallet-tracker
+
+go 1.21.6
---
+++ browser-extension/src/popup/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <hello-world />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: { HelloWorld }
+};
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
